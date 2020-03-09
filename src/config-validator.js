dc524098f596c2291909c5244145a801d2ccbb77
Panda (Thomas) Attwood <thomas.attwood@stfc.ac.uk>
2020-03-09 15:31:42 +0000

    Fix validation output

--- src/config-validator.js
+++ src/config-validator.js
@@ -8,8 +8,8 @@ const validate = ajv.compile(config);
 
 module.exports = function(data) {
   if(!validate(data)) {
-    console.error('Invalid configuration provided.', ajv.errorsText());
-    throw 'Invalid configuration provided.';
+    console.error('Invalid configuration provided: ' + ajv.errorsText(validate.errors));
+    throw new Error('Invalid configuration provided.');
   }
 };
 
